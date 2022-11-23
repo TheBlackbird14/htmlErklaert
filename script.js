e9e3b78feb1314949381567970555489b0ac0d5b
TheBlackbird14 <90150013+TheBlackbird14@users.noreply.github.com>
2022-11-23 17:12:22 +0100

    dark mode

---
+++ script.js
@@ -0,0 +1,9 @@
+function ToggleDarkMode() {
+    var element = document.body;
+    element.classList.toggle("dark");
+}
+
+function ToggleLightMode() {
+    var element = document.body;
+    element.classList.toggle("light")
+}
